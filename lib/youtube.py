f07cb881288e0cd06ee01c1cf4061fe190a06161
chaeri93 <chaeri93@ewhain.net>
2024-05-18 14:16:07 +0900

    [feat] 노래방 링크 연결

--- lib/youtube.py
+++ lib/youtube.py
@@ -1,6 +1,4 @@
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from oauth2client.tools import argparser
 
 from lib.settings import YOUTUBE_API_KEY
 
@@ -15,7 +13,7 @@ youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_SERVICE_VERSION, developer
 def crawl_youtube_image(singer, song):
     # q에 원하는 채널 이름 넣는다
     search_response = youtube.search().list(
-        q=f"{singer}+{song}",
+        q=f"tj {singer} {song}",
         order='relevance',
         part='snippet',
         maxResults=50,
@@ -23,6 +21,5 @@ def crawl_youtube_image(singer, song):
 
     youtube_image = search_response['items'][0]['snippet']['thumbnails']['default']['url']
     youtube_link = f"https://www.youtube.com/watch?v={search_response['items'][0]['id']['videoId']}"
-    print(youtube_image, youtube_link)
     return youtube_image, youtube_link
 
